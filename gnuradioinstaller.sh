f6233bd4e0c6fd96c376e8149039ec331b3133c7
Swrangsar Basumatary <swrangsarb@gmail.com>
2013-05-31 10:37:33 +0530

    committed at 2013053110:37:33

---
+++ gnuradioinstaller.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+prompt="[swrangsar]:"
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+echo "${prompt} changing directory to dports..."
+cd /opt/local/var/macports/sources/svn.macports.org/trunk/dports/
+echo "${prompt} starting svn update..."
+svn update
+echo "${prompt} starting portindex..."
+portindex
+echo "${prompt} starting gnuradio installation"
+port -v install qwt52
+port -v install py27-pyqwt
+port -v install gnuradio +full configure.compiler=llvm-gcc-4.2
+port -v install rtl-sdr
+port -v install wget git-core octave ruby lynx libosip2
+cd -
