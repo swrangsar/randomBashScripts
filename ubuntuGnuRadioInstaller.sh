3c0141713da0b9ef73ce0f1f337f7864c9d69b2d
swrangsar <swrangsar@spannbhakt-desktop.(none)>
2013-05-31 13:02:59 +0530

    committed at 2013053113:02:59

---
+++ ubuntuGnuRadioInstaller.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $UID != 0 ]]; then
+echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+apt-get -y install git-core autoconf automake  libtool g++ python-dev swig \
+pkg-config libboost-all-dev libfftw3-dev libcppunit-dev libgsl0-dev \
+libusb-dev sdcc libsdl1.2-dev python-wxgtk2.8 python-numpy \
+python-cheetah python-lxml doxygen python-qt4 python-qwt5-qt4 libxi-dev \
+libqt4-opengl-dev libqwt5-qt4-dev libfontconfig1-dev libxrender-dev 
+
+cd ~
+wget http://gnuradio.org/releases/gnuradio/gnuradio-3.6.4.2.tar.gz
+tar -xvfz gnuradio-3.6.4.2.tar.gz
+cd gnuradio-3.6.4.2
+#mkdir build
+#make
+#make test
+cd ~
