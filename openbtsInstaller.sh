40f2250acf76b402b2fa5e3fcf05b54d24c0ab05
swrangsar basumatary <swrangsar@swrangsar-desktop.(none)>
2013-06-06 19:54:11 +0530

    commit at 20130606195411

--- openbtsInstaller.sh
+++ openbtsInstaller.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
 
-sudo apt-get install asterisk autoconf libtool libosip2-dev libortp-dev libusb-1.0-0-dev g++ sqlite3 libsqlite3-dev erlang libreadline6-dev libboost1.48-all-dev
+sudo apt-get install ubuntu-restricted-extras asterisk autoconf libtool libosip2-dev libortp-dev libusb-1.0-0-dev g++ sqlite3 libsqlite3-dev erlang libreadline6-dev libboost1.48-all-dev
+
 
 cd ~
-svn co http://wush.net/svn/range/software/public OpenBTS
-cd OpenBTS/openbts/trunk
+mkdir OpenBTS
+cd OpenBTS
+svn co http://wush.net/svn/range/software/public
+
+
+# build openbts
+cd ~/OpenBTS/public/openbts/trunk
 autoreconf -i
 ./configure --with-uhd --with-resamp
 make
 cd apps
 ln -s ../Transceiver52M/transceiver .
 
-cd ~/OpenBTS/openbts/trunk
+# configure openbts
+cd ~/OpenBTS/public/openbts/trunk
 sudo rm -rf /etc/OpenBTS
 sudo mkdir /etc/OpenBTS
 sudo sqlite3 -init ./apps/OpenBTS.example.sql /etc/OpenBTS/OpenBTS.db ".quit"
 sqlite3 /etc/OpenBTS/OpenBTS.db .dump
 cd apps
-sudo ./OpenBTS
+# sudo ./OpenBTS
+
+
+# set up the subscriber registry
+cd ~/OpenBTS/public/
+cd subscriberRegistry/trunk/configFiles/
+sudo mkdir -p /var/lib/asterisk/sqlite3dir
+sudo sqlite3 -init subscriberRegistryInit.sql /var/lib/asterisk/sqlite3dir/sqlite3.db ".quit"
+sudo mkdir /var/run/OpenBTS
+
+
+# build sipauthserve
+cd ~/OpenBTS/public/subscriberRegistry/trunk
+make
+
+# configure sipauthserve
+sudo sqlite3 -init sipauthserve.example.sql /etc/OpenBTS/sipauthserve.db ".quit"
+
+
+# build smqueue
+cd ~/OpenBTS/public/smqueue/trunk
+autoreconf -i
+./configure
+make
+
+# configure smqueue
+sudo sqlite3 -init smqueue/smqueue.example.sql /etc/OpenBTS/smqueue.db ".quit"
