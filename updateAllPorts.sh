b8ee780b7359963448547d199ee580ee5d89e9c1
Swrangsar Basumatary <swrangsarb@gmail.com>
2013-06-06 22:52:02 +0530

    commit at 20130606225202

--- updateAllPorts.sh
+++ updateAllPorts.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
+
+sudo echo "Thanks!"
+
 prompt="[swrangsar]:"
-if [[ $UID != 0 ]]; then
-    echo "Please run this script with sudo:"
-    echo "sudo $0 $*"
-    exit 1
-fi
+# if [[ $UID != 0 ]]; then
+#    echo "Please run this script with sudo:"
+#    echo "sudo $0 $*"
+#    exit 1
+# fi
 echo "${prompt} changing directory to dports..."
 cd /opt/local/var/macports/sources/svn.macports.org/trunk/dports/
 echo "${prompt} starting svn update..."
-svn update
+sudo svn update
 echo "${prompt} starting portindex..."
-portindex
+sudo portindex
 echo "${prompt} starting port upgrade..."
-port upgrade outdated
+sudo port upgrade outdated
 echo "${prompt} Done upgrading all the macports :)"
 cd -
