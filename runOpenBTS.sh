70a95f179710b9bd048f54eb37dc93d0fe887f45
swrangsar basumatary <swrangsar@swrangsar-desktop.(none)>
2013-06-06 20:03:03 +0530

    commit at 20130606200303

---
+++ runOpenBTS.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo asterisk -r
+cd ~/OpenBTS/public/smqueue/trunk/smqueue
+sudo ./smqueue
+
+cd ~/OpenBTS/public/subscriberRegistry/trunk/
+sudo ./sipauthserve
+
+cd ~/OpenBTS/public/openbts/trunk/apps
+sudo ./OpenBTS
+
+cd ~/OpenBTS/public/openbts/trunk/apps
+sudo ./OpenBTSCLI
