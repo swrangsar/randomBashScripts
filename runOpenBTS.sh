a21429f69b1dc63b54daa0c6ebe6e82279950801
Swrangsar Basumatary <swrangsarb@gmail.com>
2013-06-17 15:06:53 +0530

    commit at 20130617150652

--- runOpenBTS.sh
+++ runOpenBTS.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-sudo echo "LOL"
+sudo echo "Hi, this script is starting OpenBTS!"
 sudo gnome-terminal -x sh -c "sudo asterisk -r" &
 
-cd ~/OpenBTS/public/smqueue/trunk/smqueue
-sudo gnome-terminal -x sh -c "sudo ./smqueue" &
-
-cd ~/OpenBTS/public/subscriberRegistry/trunk/
-sudo gnome-terminal -x sh -c "sudo ./sipauthserve" &
-
-cd ~/OpenBTS/public/openbts/trunk/apps
-sudo gnome-terminal -x sh -c "sudo ./OpenBTS" &
-
-cd ~/OpenBTS/public/openbts/trunk/apps
-sudo gnome-terminal -x sh -c "sudo ./OpenBTSCLI" &
+cd ~/OpenBTS/public/
+sudo gnome-terminal --tab -e "sudo smqueue/trunk/smqueue/smqueue" \
+    --tab -e "sudo subscriberRegistry/trunk/sipauthserve" \
+        --tab -e "sudo openbts/trunk/apps/OpenBTS" \
+            --tab -e "sudo openbts/trunk/apps/OpenBTSCLI" &
