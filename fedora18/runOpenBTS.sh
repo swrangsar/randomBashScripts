a000db2d14966211fb46f40026b6b7caed47b023
Swrangsar Basumatary <swrangsarb@gmail.com>
2013-06-21 13:38:11 +0530

    commit at 20130621133811

--- fedora18/runOpenBTS.sh
+++ fedora18/runOpenBTS.sh
@@ -2,10 +2,6 @@
 
 sudo echo "Hi, this script is starting OpenBTS in Fedora!"
 sudo service asterisk restart
-<<<<<<< HEAD
-sudo chkconfig asterisk on
-=======
->>>>>>> 6101a4b61d580d77b61a53142dbc31afebe6a934
 sudo gnome-terminal -x sh -c "sudo asterisk -r" &
 
 cd ~/openBTS/
@@ -14,10 +10,5 @@ sudo gnome-terminal --tab -e "sudo smqueue/trunk/smqueue/smqueue" \
 
 cd openbts/trunk/apps/
 sudo gnome-terminal --tab -e "sudo ./OpenBTS" \
-<<<<<<< HEAD
 	--tab -e "sudo ./OpenBTSCLI" &
 cd ~
-=======
-     --tab -e "sudo ./OpenBTSCLI" &
-cd ~
->>>>>>> 6101a4b61d580d77b61a53142dbc31afebe6a934
