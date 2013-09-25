cf9a1d7ffab775f6b8db9e29fba46daedb093524
Swrangsar Basumatary <swrangsarb@gmail.com>
2013-09-25 18:34:25 +0530

    commit at 20130925183425

--- gitSyncSwrangsar.sh
+++ gitSyncSwrangsar.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+
+git add --all .
+git commit -am "commit at $(date +%Y%m%d%H%M%S)"
 echo "...pulling..."
 git pull
 echo "...pushing..."
-git add --all *
-git commit -am "commit at $(date +%Y%m%d%H%M%S)"
 git push -u origin master
